b6e90e29f24305b357aeeb92e3791bedcc2ae7f0
renae carter <rkwalker89@gmail.com>
2022-07-06 15:39:17 -0400

    commit tree f8d0bb5ed38df58cf169ea02cd28eece377ea84a

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  "Hello!".toUpperCase();
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  "Hello!".toLowerCase();
+function logShout(string) {
+  console.log() string.toUpperCase();
+  }
+  "Hello!".toUpperCase();
